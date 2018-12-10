08bb5d3e2af84d6a73689353aebb44eb394fef2d
lydsnyder <lrsbp7@mail.missouri.edu>
2018-12-10 15:50:19 -0600

    Create CIFinalProject.m
    Starter file with steps :)

---
+++ CIFinalProject.m
@@ -0,0 +1,15 @@
+% CS 4770 - Computational Intelligence
+% Group 3
+% K-nearest Neighbor
+% Fall 2018
+
+% Lia ???
+% Lydia Snyder
+
+%% Import the data %%
+
+%% Separate the data into Test and Reference Data %%
+
+%% Run K-nearest Neighbor %%
+
+%% Display Output %%
